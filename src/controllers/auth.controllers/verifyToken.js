097d02b2eb5e9873798b5327a84bc87869b244a0
xezernn <xezernovruz79@gmail.com>
2024-09-22 18:02:13 +0400

    username edit (verify token)

--- src/controllers/auth.controllers/verifyToken.js
+++ src/controllers/auth.controllers/verifyToken.js
@@ -1,5 +1,5 @@
 const verifyToken = (req, res) => {
-    res.status(200).send({ message: 'Token duzdu.', status: true, user_login: req.user.login , role: req.user.role});
+    res.status(200).send({ message: 'Token duzdu.', status: true, user_login: req.user.username , role: req.user.role});
 };
 
 module.exports = verifyToken;
